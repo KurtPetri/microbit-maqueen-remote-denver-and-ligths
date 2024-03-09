8e87232154d5444ad0b20a1e9bd035a105daf118
Kurt Petri Petersen <petrisoft@gmail.com>
2024-03-09 18:02:03 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://kurtpetri.github.io/microbit-maqueen-remote-denver-and-ligths/](https://kurtpetri.github.io/microbit-maqueen-remote-denver-and-ligths/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/kurtpetri/microbit-maqueen-remote-denver-and-ligths** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/kurtpetri/microbit-maqueen-remote-denver-and-ligths** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,90 @@
+IR.IR_callbackUser(function (number) {
+    if (number == forwardKey) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        driveForward(speed)
+    }
+    // Bak
+    if (number == backwardKey) {
+        driveBackward(speed)
+maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    }
+    // Drej til højre
+    if (number == leftKey) {
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        turnLeft(turnSpeed)
+    }
+    // Drej til venstre
+    if (number == rightKey) {
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        turnRight(turnSpeed)
+    }
+    if (number == speedUpKey) {
+        speedUp(speed)
+    }
+    if (number == speedDownKey) {
+        speedDown(speed)
+    }
+    // Stop
+    if (number == stopKey || number == stopKey2) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    // turn off lights
+    if (number == stopKey) {
+        strip.clear()
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    }
+    function driveForward(speed: number) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
+    }
+function turnLeft(speed: number) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, speed)
+    }
+function driveBackward(speed: number) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, speed)
+    }
+function turnRight(speed: number) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, speed)
+    }
+function speedUp(speed: number) {
+        speed = speed + 1;
+    }
+function speedDown(speed: number) {
+        speed = speed - 1;
+        if (speed < 10)
+            speed = 10;
+    }
+})
+let stopKey2 = 0
+let stopKey = 0
+let rightKey = 0
+let leftKey = 0
+let backwardKey = 0
+let forwardKey = 0
+let speedUpKey = 0
+let speedDownKey = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+strip.showRainbow(1, 360)
+strip.show()
+let speed = 40
+let turnSpeed = 25
+speedDownKey = 65
+speedUpKey = 66
+forwardKey = 11
+backwardKey = 14
+leftKey = 17
+rightKey = 16
+stopKey = 10
+stopKey2 = 13
+basic.showIcon(IconNames.Ghost)
+basic.forever(function () {
+    strip.rotate(1)
+    strip.show()
+    basic.pause(100)
+})
